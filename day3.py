703cda41712d43dea2877731c68c99d0bed7f4eb
Vibhanshu Kumar Shubham <120111312+Theubaa@users.noreply.github.com>
2024-09-04 05:20:21 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,27 @@
+"""
+SUM OF DIGIT STRING AFTER CONVERT
+
+You are given a string s consisting of lowercase English letters, and an integer k.
+
+First, convert s into an integer by replacing each letter with its position in the alphabet (i.e., replace 'a' with 1, 'b' with 2, ..., 'z' with 26). Then, transform the integer by replacing it with the sum of its digits. Repeat the transform operation k times in total.
+
+For example, if s = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
+
+Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
+Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
+Transform #2: 17 ➝ 1 + 7 ➝ 8
+Return the resulting integer after performing the operations described above.
+"""
+
+#  CODE 
+
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        
+        num_str = ''.join(str(ord(char) - ord('a') + 1) for char in s)
+        
+       
+        for _ in range(k):
+            num_str = str(sum(int(digit) for digit in num_str))
+        
+        return int(num_str)
